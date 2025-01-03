e1a43809e4cd2059be2b0275dff690bf8f589d41
Brian Lookabaugh <brianlookabaugh21@gmail.com>
2025-01-03 10:40:13 -0600

    Updating sticker again

--- images/website_badge_code.R
+++ images/website_badge_code.R
@@ -17,11 +17,11 @@ node_colors <- c(
 )
 
 dgp1_dag <- dagitty('dag {
-  "Z[TIC]" [pos="2.5,2"]
-  "X[t-1]" [pos="1,1"]
-  "Y[t-1]" [pos="2,1.25"]
-  "X[t]" [pos="3,1"]
-  "Y[t]" [pos="4,1.25"]
+  "Z[TIC]" [pos="1.25,1"]
+  "X[t-1]" [pos="0.5,0.5"]
+  "Y[t-1]" [pos="1,0.75"]
+  "X[t]" [pos="1.5,0.5"]
+  "Y[t]" [pos="2,0.625"]
   "Z[TIC]" -> "X[t-1]"
   "Z[TIC]" -> "Y[t-1]"
   "Z[TIC]" -> "X[t]"
@@ -39,15 +39,15 @@ p <- ggplot(dgp1_dag, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_dag_point(aes(fill = name), color = "black", size = 16, shape = 21) +  
   scale_fill_manual(values = node_colors) +  
   theme_dag() +
-  guides(fill = "none") 
+  guides(fill = "none")
 
 sticker(
   subplot = p,
   package = "",
-  s_width = 1.25,
-  s_height = 1.25,
-  s_x = 1,
-  s_y = 1,
+  s_width = 1.65,
+  s_height = 1.7,
+  s_x = 1.05,
+  s_y = 1.15,
   h_fill = "white",
   h_color = "black"
   ) %>%
